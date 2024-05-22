704cd45616981b98f4b1a4a8c57bd033f021c822
msg4edson <msg.edson@gmail.com>
2024-05-22 09:53:21 -0300

    add files

---
+++ acronyms.py
@@ -0,0 +1,11 @@
+look_up = input('What software acronym would you like to look up?\n')
+
+found = False
+with open('acronyms.txt') as file:
+    for line in file:
+        if look_up in line:
+            print(line)
+            found = True
+            break
+if not found:
+    print('The acronym does not exist')
