b85e7640d5b698d20089480813f91255374d645c
msg.edson <msg.edson@gmail.com>
2024-05-22 20:51:52 -0300

    add final script

--- acronyms.py
+++ acronyms.py
@@ -1,11 +1,33 @@
-look_up = input('What software acronym would you like to look up?\n')
+def find_acronym():
+    look_up = input('What software acronym would you like to look up?\n')
 
-found = False
-with open('acronyms.txt') as file:
-    for line in file:
-        if look_up in line:
-            print(line)
-            found = True
-            break
-if not found:
-    print('The acronym does not exist')
+    found = False
+    try:
+        with open('acronyms.txt') as file:
+            for line in file:
+                if look_up in line:
+                    print(line)
+                    found = True
+                    break
+    except FileNotFoundError as e:
+        print('File not found')
+        return
+    
+    if not found:
+        print('The acronym does not exist')
+
+def add_acronym():
+    acronym = input("What acronym do you want to add?\n")
+    definition = input("What is the definition?\n")
+    with open("acronyms.txt", 'a') as file:
+        file.write(acronym + ' - ' + definition + '\n')
+
+def main():
+    # Ask the user whether they want do add or find an acronym
+    choice = input('Do you whant to find(F) or add(A) an acronym?\n')
+    if choice == 'F':
+        find_acronym()
+    elif choice == 'A':
+        add_acronym()
+
+main()
---
+++ temp.py
@@ -0,0 +1,7 @@
+acronym = input("What acronym do you want to add?\n")
+definition = input("What is the definition?\n")
+with open("acronyms.txt", 'a') as file:
+
+    file.write(acronym + ' - ' + definition + '\n')
+
+
